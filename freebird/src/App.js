bead0ea9f88981e3676ba3f237e26e5f8d6ffd2c
Manhart, John <manhartjohn@gmail.com>
2018-07-30 20:38:51 -0500

    Adding in react-create app

---
+++ freebird/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">🇺🇸</h1>
+          <h1 className="App-title">How many Freebirds?</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+
