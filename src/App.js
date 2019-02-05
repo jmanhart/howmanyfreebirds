f7c4de66b6340464863868d671ecc10b639d3e6d
Manhart, John <manhartjohn@gmail.com>
2019-02-04 21:15:01 -0600

    Adding in some cool styles

--- src/App.js
+++ src/App.js
@@ -13,24 +13,39 @@ export default class App extends Component {
 
   handleSubmit(event){
     event.preventDefault();
-    
-  
-    let door = this.refs.message.value * 60;
 
-    let val = door / songLength;
+    let timeInSeconds = (this.refs.message.value * 60);
+    let calc = timeInSeconds / songLength
+    let msg;
 
+    // More than one listen
+    if (timeInSeconds > songLength) {
+      msg = "You can listen to Freebird " + calc.toFixed() + " times 🤘";
+    } 
 
-    console.log(val);
-
-    if (songLength < val){
-      val = "You can listen to Freebird " + val.toFixed(2) + " times 🤘";
-    } else if (val < songLength) {
-      val = "NO " + val.toFixed(2) + " times 🤘";
+    // Less than one listen
+    if (timeInSeconds < songLength) {
+      let remainder = (songLength - timeInSeconds) / 60;
+      msg = "Dang! You need " + remainder.toFixed() + " more minutes to be free as a bird now.";
     }
 
-    
+    // Zero time for Freebird
+    if (timeInSeconds == 0) {
+      msg = "Get serious son! Find time for that sweet sweet swamp music!"
+    }
 
-    this.setState({value: val});
+    // Easter Egg - 69
+    if (this.refs.message.value == 69) {
+      msg = "Giggidy Giggidy 😉...You can listen to Freebird " + calc.toFixed() + " times."
+    }
+
+    // Easter Egg - 420
+    if (this.refs.message.value == 420) {
+      msg = "Whooa thats hella meta...You can listen to Freebird " + calc.toFixed() + " times."
+    }
+
+    console.log(calc);
+    this.setState({value: msg});
 
   }
 
@@ -38,17 +53,16 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">🇺🇸</h1>
-          <h1 className="App-title">How many Freebirds?</h1>
+        <iframe src="https://giphy.com/embed/ayfwIEB2hvoDm" width="100" height="100" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
           <form
               onSubmit={this.handleSubmit}>
               <input
                   ref="message"
-                  placeholder="Send a message..."
+                  placeholder="How many minutes do you need to be free?"
                   onSubmit={this.handleSubmit}
                   />
           </form>
-          {this.state.value}
+          <h1 className="App-title">{this.state.value}</h1>
         </header>
 
       </div>
