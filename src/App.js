47f8ae8787658753099a59ab31e9be9803c38723
Manhart, John <manhartjohn@gmail.com>
2019-02-06 20:46:22 -0600

    Adding in some more basic styles

--- src/App.js
+++ src/App.js
@@ -7,17 +7,50 @@ import FreeBirdForm  from './components/form/index.js';
 import './App.css';
 
 export default class App extends Component {
+  
   render() {
+    console.log(
+      `
+      𝕴𝖋 𝕴 𝖑𝖊𝖆𝖛𝖊 𝖍𝖊𝖗𝖊 𝖙𝖔𝖒𝖔𝖗𝖗𝖔𝖜
+      𝖂𝖔𝖚𝖑𝖉 𝖞𝖔𝖚 𝖘𝖙𝖎𝖑𝖑 𝖗𝖊𝖒𝖊𝖒𝖇𝖊𝖗 𝖒𝖊?
+      𝕱𝖔𝖗 𝕴 𝖒𝖚𝖘𝖙 𝖇𝖊 𝖙𝖗𝖆𝖛𝖊𝖑𝖎𝖓𝖌 𝖔𝖓, 𝖓𝖔𝖜
+      𝕮𝖆𝖚𝖘𝖊 𝖙𝖍𝖊𝖗𝖊'𝖘 𝖙𝖔𝖔 𝖒𝖆𝖓𝖞 𝖕𝖑𝖆𝖈𝖊𝖘 𝕴'𝖛𝖊 𝖌𝖔𝖙 𝖙𝖔 𝖘𝖊𝖊
+      𝕭𝖚𝖙, 𝖎𝖋 𝕴 𝖘𝖙𝖆𝖞𝖊𝖉 𝖍𝖊𝖗𝖊 𝖜𝖎𝖙𝖍 𝖞𝖔𝖚, 𝖌𝖎𝖗𝖑
+      𝕿𝖍𝖎𝖓𝖌𝖘 𝖏𝖚𝖘𝖙 𝖈𝖔𝖚𝖑𝖉𝖓'𝖙 𝖇𝖊 𝖙𝖍𝖊 𝖘𝖆𝖒𝖊
+      𝕮𝖆𝖚𝖘𝖊 𝕴'𝖒 𝖆𝖘 𝖋𝖗𝖊𝖊 𝖆𝖘 𝖆 𝖇𝖎𝖗𝖉 𝖓𝖔𝖜
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖓𝖔𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕺𝖍, 𝖔𝖍, 𝖔𝖍, 𝖔𝖍, 𝖔𝖍
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖓𝖔𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖓𝖔𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕷𝖔𝖗𝖉 𝖐𝖓𝖔𝖜𝖘, 𝕴 𝖈𝖆𝖓'𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+
+      𝕭𝖞𝖊, 𝖇𝖞𝖊, 𝖇𝖆𝖇𝖞, 𝖎𝖙'𝖘 𝖇𝖊𝖊𝖓 𝖆 𝖘𝖜𝖊𝖊𝖙 𝖑𝖔𝖛𝖊
+      𝕿𝖍𝖔𝖚𝖌𝖍 𝖙𝖍𝖎𝖘 𝖋𝖊𝖊𝖑𝖎𝖓𝖌 𝕴 𝖈𝖆𝖓'𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕭𝖚𝖙 𝖕𝖑𝖊𝖆𝖘𝖊 𝖉𝖔𝖓'𝖙 𝖙𝖆𝖐𝖊 𝖎𝖙 𝖘𝖔 𝖇𝖆𝖉𝖑𝖞
+      𝕮𝖆𝖚𝖘𝖊 𝕷𝖔𝖗𝖉 𝖐𝖓𝖔𝖜𝖘 𝕴'𝖒 𝖙𝖔 𝖇𝖑𝖆𝖒𝖊
+      𝕭𝖚𝖙, 𝖎𝖋 𝕴 𝖘𝖙𝖆𝖞𝖊𝖉 𝖍𝖊𝖗𝖊 𝖜𝖎𝖙𝖍 𝖞𝖔𝖚, 𝖌𝖎𝖗𝖑
+      𝕿𝖍𝖎𝖓𝖌𝖘 𝖏𝖚𝖘𝖙 𝖈𝖔𝖚𝖑𝖉𝖓'𝖙 𝖇𝖊 𝖙𝖍𝖊 𝖘𝖆𝖒𝖊
+      𝕮𝖆𝖚𝖘𝖊 𝕴'𝖒 𝖆𝖘 𝖋𝖗𝖊𝖊 𝖆𝖘 𝖆 𝖇𝖎𝖗𝖉 𝖓𝖔𝖜
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚'𝖑𝖑 𝖓𝖊𝖛𝖊𝖗 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕺𝖍, 𝖔𝖍, 𝖔𝖍, 𝖔𝖍, 𝖔𝖍
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖓𝖔𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕬𝖓𝖉 𝖙𝖍𝖎𝖘 𝖇𝖎𝖗𝖉 𝖞𝖔𝖚 𝖈𝖆𝖓 𝖓𝖔𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕷𝖔𝖗𝖉 𝖐𝖓𝖔𝖜𝖘, 𝕴 𝖈𝖆𝖓'𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕷𝖔𝖗𝖉 𝖍𝖊𝖑𝖕 𝖒𝖊, 𝕴 𝖈𝖆𝖓'𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝕷𝖔𝖗𝖉, 𝕴 𝖈𝖆𝖓'𝖙 𝖈𝖍𝖆𝖓𝖌𝖊
+      𝖂𝖔𝖓'𝖙 𝖞𝖔𝖚 𝖋𝖑𝖞 𝖍𝖎𝖌𝖍, 𝖋𝖗𝖊𝖊 𝖇𝖎𝖗𝖉, 𝖞𝖊𝖆𝖍
+      `
+    );
     return (
       <div className="App">
-        <header className="App-header">
         <div>
-        <iframe src="https://giphy.com/embed/ayfwIEB2hvoDm" width="50" height="50" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <h1 className="App-title">How many minutes do you need to be free?</h1>
-        <FreeBirdForm  />
+        <iframe title="America" src="https://giphy.com/embed/ayfwIEB2hvoDm" width="50" height="50" frameBorder="0"  allowFullScreen />
+          <h1 className="App-title">How many free minutes do you have?</h1>
+          <FreeBirdForm  />
         </div>
-        </header>
       </div>
     );
   }
 }
+
+/* <iframe title="America" src="https://giphy.com/embed/ayfwIEB2hvoDm" width="50" height="50" frameBorder="0"  allowFullScreen /> */
--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 
 // Importing Styles
@@ -10,71 +10,111 @@ let songLength = 546;
 export default class FreeBirdForm extends React.Component {
     constructor() {
         super()
-        this.state = {value:[]};
+        this.state = { value:[] };
         this.handleSubmit = this.handleSubmit.bind(this)
+        this.baseState = this.state 
       }
-      handleSubmit(event){
-        event.preventDefault();
 
-        // Convert time to seconds
-        let timeInSeconds = (this.refs.message.value * 60);
-        // Find the amount of time for number of free birds
-        let calc = timeInSeconds / songLength
-        let msg;
     
-        // More than one listen
-        if (timeInSeconds > songLength) {
-          msg = "Great you can listen to Freebird " + calc.toFixed() + " times 🤘";
-        } 
-    
-        // Less than one listen
-        if (timeInSeconds < songLength) {
-          let remainder = (songLength - timeInSeconds) / 60;
-          msg = "Dang! You need " + remainder.toFixed() + " more minutes to be free as a bird.";
-        }
-    
-        // Zero time for Freebird
-        if (timeInSeconds == 0) {
-          msg = "Get serious son! Find time for that sweet sweet swamp music!"
-        }
-    
-        // Easter Egg - 69
-        if (this.refs.message.value == 69) {
-          msg = "Giggidy Giggidy 😉...You can listen to Freebird " + calc.toFixed() + " times."
-        }
-    
-        // Easter Egg - 420
-        if (this.refs.message.value == 420) {
-          msg = "You can listen to Freebird " + calc.toFixed() + " times."
-        }
 
-        // Easter Egg - 10
-        if (this.refs.message.value == 10) {
-            msg = "Whooa thats hella meta...You can listen to Freebird " + calc.toFixed() + " time. 🤘"
-        }
+    handleSubmit(event){
+      event.preventDefault();
 
-        // Easter Egg - Infinity ∞
-        if (this.refs.message.value == "∞") {
-          msg = "TIME IS A FLAT CIRCLE "
-        }
     
-        console.log(calc);
-        this.setState({value: msg});
-    
+      // Convert time to seconds
+      let timeInSeconds = (this.refs.message.value * 60);
+      // Find the amount of time for number of free birds
+      let calc = timeInSeconds / songLength
+      let msg;
+  
+      // More than one listen
+      if (timeInSeconds > songLength) {
+        msg = "Great you can listen to Freebird " + calc.toFixed() + " freaking times 🤘";
+      } 
+  
+      // Less than one listen
+      if (timeInSeconds < songLength) {
+        let remainder = (songLength - timeInSeconds) / 60;
+        msg = "Dang! You need " + remainder.toFixed() + " more minutes to be free as a bird.";
       }
+  
+      // Zero time for Freebird
+      if (timeInSeconds == 0) {
+        msg = "You need to find some time to fly high. "
+      }
+  
+      // Easter Egg - 69
+      if (this.refs.message.value == 69) {
+        msg = "Giggidy Giggidy 😉...You can listen to Freebird " + calc.toFixed() + " times."
+      }
+  
+      // Easter Egg - 420
+      if (this.refs.message.value == 420) {
+        msg = "Maybe the smoke will help. You can listen to Freebird " + calc.toFixed() + " times."
+      }
+
+      // Easter Egg - 10
+      if (this.refs.message.value == 10) {
+          msg = "You can listen to Freebird " + calc.toFixed() + " time. 🤘"
+      }
+
+      // Easter Egg - Infinity ∞
+      if (this.refs.message.value == "∞") {
+        msg = "TIME IS A FLAT CIRCLE "
+      }
+
+      // Easter Egg - 1000
+      if (this.refs.message.value > 999) {
+        msg = "Whoa you really like Freebird " + calc.toFixed()
+      }
+      
+      this.setState({value: msg});
+  
+    }
+
+
+    updateInput = val => { 
+      this.refs.message.value = ''
+    }
+
+    resetForm = () => {
+      this.setState(this.baseState)
+      this.updateInput() 
+    }
     
+   
+
   render() {
     return (
         <form
         onSubmit={this.handleSubmit}>
             <input
-            ref="message"
-            placeholder="mins"
-            onSubmit={this.handleSubmit}
+              ref="message"
+              placeholder="mins"
+              onSubmit={this.handleSubmit}
+              maxlength="5" 
             />
+
             <h1 className="App-title">{this.state.value}</h1>
+
         </form>
    
     );
   }
 }
+
+/*
+            <div class="cursor">
+              <input
+              ref="message"
+              placeholder="mins"
+              onSubmit={this.handleSubmit}
+              />
+              <i></i>
+            </div>
+
+                        <button
+              onClick={this.resetForm}
+              > X
+            </button>
+*/
